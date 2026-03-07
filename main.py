c04f904e384119514460df1cfea93b967207d4b6
Zachary Orr <516458+ZachOrr@users.noreply.github.com>
2026-03-07 18:31:35 -0500

    New secret management + CD, so I don't have to manage deploys myself

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import json
 import logging
+import os
 
 import google.cloud.logging
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, Response
-from google.cloud import ndb
+from google.cloud import ndb, secretmanager
 
 from models.notification import Notification
 
+load_dotenv()
 
-SECRET = "REPLACE_WITH_SECRET"
+
+def get_secret(secret_id):
+    """Load a secret from GCP Secret Manager, falling back to environment variables for local dev."""
+    if os.environ.get(secret_id):
+        return os.environ[secret_id]
+    project_id = os.environ.get(
+        "GOOGLE_CLOUD_PROJECT", os.environ.get("GCLOUD_PROJECT")
+    )
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+    return client.access_secret_version(name=name).payload.data.decode("utf-8")
+
+
+SECRET = get_secret("TBA_SECRET")
 
 logging_client = google.cloud.logging.Client()
 logging_client.setup_logging()
